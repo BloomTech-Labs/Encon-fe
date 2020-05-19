fe534558a50028f18477402804e74ed0e2468eeb
Alex Martinez <alexmartinez0071234@gmail.com>
2020-05-19 12:08:49 -0400

    added in the radio buttons for the form

--- encon/src/App.js
+++ encon/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './App.scss';
-import { Nav } from './components/Nav.js';
-import { Route } from 'react-router-dom';
-import { LandingPage } from './components/Landing-Page';
+import React from "react";
+import "./App.scss";
+import { Nav } from "./components/Nav.js";
+import { Route } from "react-router-dom";
+import { LandingPage } from "./components/Landing-Page";
+import { Calculator } from "./components/Calculator";
 
 export const App = () => {
-	return (
-		<div className='App'>
-			<Nav />
-			<Route path='/'>
-				<LandingPage />
-			</Route>
-			<p>encon wow</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Nav />
+      <Route path="/">
+        <LandingPage />
+      </Route>
+      <Calculator />
+      <p>encon wow</p>
+    </div>
+  );
 };
---
+++ encon/src/components/Calculator.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Calculator = () => {
+  return (
+    <div>
+      <form>
+        <label>TV</label>
+        <input type="radio" value="TV" />
+
+        <label>Phone</label>
+        <input type="radio" value="Phone" />
+
+        <label>Washer/Dryer</label>
+        <input type="radio" value="Washer/Dryer" />
+
+        <label>Mircowave</label>
+        <input type="radio" value="Microwave" />
+
+        <label>Fridge/Freezer</label>
+        <input type="radio" value="Fridge/Freezer" />
+
+        <label>AC Unit</label>
+        <input type="radio" value="AC Unit" />
+      </form>
+    </div>
+  );
+};
