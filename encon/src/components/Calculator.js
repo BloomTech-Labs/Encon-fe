dcdb0fb96967337cbebb63f3fe6014432145be96
Alex Martinez <alexmartinez0071234@gmail.com>
2020-05-19 17:05:44 -0400

    styled and made radio buttons

--- encon/src/components/Calculator.js
+++ encon/src/components/Calculator.js
@@ -3,18 +3,37 @@ import "../components/Calculator.scss";
 export const Calculator = () => {
   return (
     <div>
+      {/* Calculator Starts here and is a form itself */}
       <form className="calculator">
         <h3>Device</h3>
-        <label>
-          TV
-          <input className="radio" type="radio" value="TV" />
-        </label>
-
+        {/* All the radio buttons begin here */}
+        <div className="radioButtons">
+          <label className="radio">
+            Computer
+            <input type="radio" value="Computer" />
+          </label>
+          <label className="radio">
+            Washing Machine
+            <input type="radio" value="Washing Machine" />
+          </label>
+          <label className="radio">
+            Refrigerator
+            <input type="radio" value="Washer/Dryer" />
+          </label>
+          <label className="radio">
+            TV
+            <input type="radio" value="TV" />
+          </label>
+          <label className="radio">
+            Lighting
+            <input type="radio" value="Lighting" />
+          </label>
+        </div>
+        {/* Radio Buttons end here */}
         <label>
           Location/Zip Code
           <input type="text" />
         </label>
-
         <div>
           <h2>Daily Use</h2>
 
@@ -28,13 +47,11 @@ export const Calculator = () => {
             Minutes
           </label>
         </div>
-
         <label>
           Days Per Week
           <input type="integer" />
         </label>
       </form>
-
       <div className="calc-output">
         {/*use expression interpolation `${}` to insert output data once we connect to DS BE using axios*/}
         <h3>Total Daily Cost</h3>
