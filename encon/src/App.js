af8cbce81090c9c0d4dc642ee9cc33ddbb7d97b0
Michael Ross <mross.j@gmail.com>
2020-05-20 11:54:36 -0400

    update branches
    * updated app.js routes
    * updated login
    * updated styles

--- encon/src/App.js
+++ encon/src/App.js
@@ -1,28 +1,23 @@
-import React from "react";
-import "./App.scss";
-
-import './styles/login.scss';
-import Login from './components/LogIn.js'
-function App() {
-  return (
-    <div className="App">
-      <Login/>
-
-import { Nav } from "./components/Nav.js";
-import { Route } from "react-router-dom";
-import { LandingPage } from "./components/Landing-Page";
-import { Calculator } from "./components/Calculator";
+import React from 'react';
+import './App.scss';
+import { Login } from './components/LogIn.js';
+import { Nav } from './components/Nav.js';
+import { Route } from 'react-router-dom';
+import { LandingPage } from './components/Landing-Page';
+import { Calculator } from './components/Calculator';
 
 export const App = () => {
-  return (
-    <div className="App">
-      <Nav />
-      <Route path="/">
-        <LandingPage />
-      </Route>
-      <Calculator />
-      <p>encon wow</p>
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Nav />
+			<Route exact path='/'>
+				<LandingPage />
+				<Calculator />
+			</Route>
+			<Route path='/login'>
+				<Login />
+			</Route>
+			<p>encon wow</p>
+		</div>
+	);
 };
--- encon/src/components/LogIn.js
+++ encon/src/components/LogIn.js
@@ -1,54 +1,49 @@
-import React, {useState} from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import '../styles/login.scss';
 
-const Login = () => {
-  const { handleSubmit, register, errors } = useForm();
-  const onSubmit = e => {
-    e.persist()
-    setUser({
+export const Login = () => {
+	const { handleSubmit, register, errors } = useForm();
+	const onSubmit = (e) => {
+		e.persist();
+		setUser({
+			...user,
+			[e.target.email]: e.target.value,
+		});
+	};
 
-        ...user,
-        [e.target.email]: e.target.value
+	const [user, setUser] = useState({
+		email: '',
+		password: '',
+	});
+	return (
+		<div className='login-container'>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<label htmlFor='email' className='label'>
+					Email
+				</label>
 
-    })
-}
+				<input
+					name='email'
+					ref={register({
+						required: 'Required',
+						pattern: {
+							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+							message: 'invalid email address',
+						},
+					})}
+				/>
+				{errors.email && errors.email.message}
+				<br />
+				<label htmlFor='password' className='label'>
+					Password
+				</label>
 
-  const [user, setUser] = useState({
-    
-    email: '',
-    password: '',
-});
-  return (
+				<input name='password' type='password' ref={register} />
+				<br />
 
- <div className='login-container'>
-    <form onSubmit={handleSubmit(onSubmit)}>
-     <label htmlFor='email'className='label'>Email</label>
-     
-      <input
-        name= 'email'
-        ref={register({
-          required: "Required",
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-            message: "invalid email address"
-          }
-        })}
-      />
-      {errors.email && errors.email.message}
-      <br/>
-    <label htmlFor='password' className='label'>Password</label>
-    
-      <input
-        name= 'password'
-        type= 'password'
-        ref={register}
-      />
-      <br/>
-
-      <button type="submit" >Sign In</button>
-    </form>
-    </div>
-  );
+				<button type='submit'>Sign In</button>
+			</form>
+		</div>
+	);
 };
-export default Login;
