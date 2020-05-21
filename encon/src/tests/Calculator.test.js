525abcf3874175a846014c11880767ab36656743
Alex Martinez <alexmartinez0071234@gmail.com>
2020-05-21 16:50:24 -0400

    removed zombie code

--- encon/src/tests/Calculator.test.js
+++ encon/src/tests/Calculator.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Calculator } from "../components/Calculator";
-import { App } from "../App";
 import { BrowserRouter as Router } from "react-router-dom";
 test("should render Calculator component ", () => {
   render(
