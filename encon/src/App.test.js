3a5e4034164dce5b00bdaf8b527511f7785596c4
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-05-22 16:26:10 -0500

    added basic test and added calc styling

--- encon/src/App.test.js
+++ encon/src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { App } from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("should render app", () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
 });
