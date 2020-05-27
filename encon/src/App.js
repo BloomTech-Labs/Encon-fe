50a818ec81a7937396cb82aa1310e4f1b955cae0
Alex Martinez <alexmartinez0071234@gmail.com>
2020-05-27 12:20:43 -0400

    formatted code and fixed font size

--- encon/src/App.js
+++ encon/src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './App.scss';
-import { Route } from 'react-router-dom';
-import { Login } from './components/Login.js';
-import { Register } from './components/Register.js';
-import { Navigation } from './components/Navigation.js';
-import { LandingPage } from './components/Landing-Page';
-import { LogoHeader } from './components/Logo-Header';
+import React from "react";
+import "./App.scss";
+import { Route } from "react-router-dom";
+import { Login } from "./components/LogIn.js";
+import { Register } from "./components/Register.js";
+import { Navigation } from "./components/Navigation.js";
+import { LandingPage } from "./components/Landing-Page";
+import { LogoHeader } from "./components/Logo-Header";
 
 const App = () => {
-	return (
-		<div className='App'>
-			<Navigation />
-			<LogoHeader />
-			<Route exact path='/'>
-				<LandingPage />
-			</Route>
-			<Route path='/login'>
-				<Login />
-			</Route>
-			<Route path='/register'>
-				<Register />
-			</Route>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navigation />
+      <LogoHeader />
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </div>
+  );
 };
 
 export default App;
--- encon/src/components/LogIn.js
+++ encon/src/components/LogIn.js
@@ -1,81 +1,81 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import '../styles/Login.scss';
-import { HeaderAlt } from '../components/Header-Alt.js';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "../styles/login.scss";
+import { HeaderAlt } from "../components/Header-Alt.js";
+import axios from "axios";
 
 export const Login = () => {
-	const { handleSubmit, register, errors } = useForm();
+  const { handleSubmit, register, errors } = useForm();
 
-	const handleChange = (e) => {
-		setUser({
-			...user,
-			[e.target.name]: e.target.value,
-		});
-	};
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		axios
-			.post('/back end user/', user)
-			.then((res) => {
-				localStorage.setItem('token', res.data.token);
-				console.log('user data', res.data.user);
-			})
-			.catch((err) => {
-				console.log('Error while logging in', err.response);
-			});
-	};
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("/back end user/", user)
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        console.log("user data", res.data.user);
+      })
+      .catch((err) => {
+        console.log("Error while logging in", err.response);
+      });
+  };
 
-	const [user, setUser] = useState({
-		email: '',
-		password: '',
-	});
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-	return (
-		<div className='login-container'>
-			<HeaderAlt />
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='email' className='label'>
-					Email
-				</label>
+  return (
+    <div className="login-container">
+      <HeaderAlt />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email" className="label">
+          Email
+        </label>
 
-				<input
-					id='email'
-					htmlFor='email'
-					className='email'
-					name='email'
-					ref={register({
-						required: 'Required',
-						pattern: {
-							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-							message: 'invalid email address',
-						},
-					})}
-					value={user.email}
-					onChange={handleChange}
-				/>
-				{errors.email && errors.email.message}
-				<br />
-				<label htmlFor='password' className='label'>
-					Password
-				</label>
+        <input
+          id="email"
+          htmlFor="email"
+          className="email"
+          name="email"
+          ref={register({
+            required: "Required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "invalid email address",
+            },
+          })}
+          value={user.email}
+          onChange={handleChange}
+        />
+        {errors.email && errors.email.message}
+        <br />
+        <label htmlFor="password" className="label">
+          Password
+        </label>
 
-				<input
-					id='password'
-					name='password'
-					type='password'
-					ref={register}
-					value={user.password}
-					onChange={handleChange}
-				/>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          ref={register}
+          value={user.password}
+          onChange={handleChange}
+        />
 
-				<br />
+        <br />
 
-				<button className='app-buttons' type='submit' data-testid='sign in'>
-					Sign In
-				</button>
-			</form>
-		</div>
-	);
+        <button className="app-buttons" type="submit" data-testid="sign in">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 };
