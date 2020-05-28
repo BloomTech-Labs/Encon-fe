2efbc28e0c0a88e1097bbf490ae974d2d92a1a5c
Alex Martinez <alexmartinez0071234@gmail.com>
2020-05-28 17:10:17 -0400

    added in the icon pack and attributed the author

---
+++ encon/src/components/mobile/Appliance-List.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const ApplianceList = () => {
+  return (
+    <div>
+      <p>WOW</p>
+      <div>
+        Icons made by{" "}
+        <a
+          href="https://www.flaticon.com/authors/nikita-golubev"
+          title="Nikita Golubev"
+        >
+          Nikita Golubev
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+};
