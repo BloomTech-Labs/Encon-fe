d664d089f2410b1c4202ab1ddc1714d03f15aedf
Ben <bbjberg91@gmail.com>
2020-06-01 17:34:51 -0400

    merge

--- encon/src/index.js
+++ encon/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter} from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+import config from './App.config.js';
 import  App  from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AppWithRouter = withRouter(App);
 
+function onAuthRequired({ history }) {
+	history.push('/login');
+  }
+
 ReactDOM.render(
 	<Router>
-		<AppWithRouter />
+		<Security issuer={config.issuer}
+		client_id={config.client_id}
+		redirect_uri={config.redirect_uri}
+		onAuthRequired={onAuthRequired}>
+			<AppWithRouter />
+		</Security>
 	</Router>,
 	document.getElementById('root')
 );
