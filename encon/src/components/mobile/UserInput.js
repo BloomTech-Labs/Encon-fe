c84e8865f6aa5ac339bf618838a7af6964f5088f
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-06-03 18:12:12 -0500

    set up uniform jsx tags and classnames for styling

--- encon/src/components/mobile/UserInput.js
+++ encon/src/components/mobile/UserInput.js
@@ -73,128 +73,148 @@ export const UserInput = () => {
       <h2>Calendar is going here</h2>
       <form onSubmit={handleSubmit}>
         <div className="device-container">
-          <section className="dishWasher-sec">
+          <section className="device-section">
             <h3>Dishwasher</h3>
-            <div className="dish-hours">
-              <input
-                type="number"
-                name="dishWasherHours"
-                ref={register({ min: 1, max: 24 })}
-                onChange={handleDishHoursChange}
-                value={dishWasher.hours}
-              ></input>
-              <label htmlFor="dishWasherHours">hours</label>
-              {errors.dishWasherHours && "hours 1-24"}
-            </div>
+            <div className="device-fields">
+              <div className="device-use-duration">
+                <input
+                  type="number"
+                  name="dishWasherHours"
+                  ref={register({ min: 1, max: 24 })}
+                  onChange={handleDishHoursChange}
+                  value={dishWasher.hours}
+                ></input>
+                <label htmlFor="dishWasherHours">hours</label>
+                {errors.dishWasherHours && "hours 1-24"}
+              </div>
 
-            <div className="dish-days">
-              <input
-                type="integer"
-                ref={register({ min: 1, max: 7 })}
-                name="dishWasherDays"
-                onChange={handleDishDaysChange}
-                value={dishWasher.days}
-              ></input>
-              <label htmlFor="dishWasher days">days per week</label>
-              {errors.dishWasherDays && <p>enter message</p>}
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 7 })}
+                  name="dishWasherDays"
+                  onChange={handleDishDaysChange}
+                  value={dishWasher.days}
+                ></input>
+                <label htmlFor="dishWasher days">days per week</label>
+                {errors.dishWasherDays && <p>enter message</p>}
+              </div>
             </div>
           </section>
 
-          <section className="washing-machine-sec">
+          <section className="device-section">
             <h3>Washing Machine</h3>
-            <div className="washing-hours">
-              <label htmlFor="Washing Machine hours">hours</label>
-              <input
-                type="integer"
-                ref={register({ min: 1, max: 24 })}
-                name="Washing Machine hours"
-                onChange={handleWashingHoursChange}
-                value={washingMachine.hours}
-              ></input>
-              {errors.entername && <p>enter message</p>}
-            </div>
+            <div className="device-fields">
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 24 })}
+                  name="Washing Machine hours"
+                  onChange={handleWashingHoursChange}
+                  value={washingMachine.hours}
+                ></input>
+                <label htmlFor="Washing Machine hours">hours</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
 
-            <div className="washing-days">
-              <label htmlFor="Washing Machine days">days per week</label>
-              <input
-                type="integer"
-                ref={register({ min: 1, max: 7 })}
-                name="Washing Machine days"
-                onChange={handleWashingDaysChange}
-                value={washingMachine.days}
-              ></input>
-              {errors.entername && <p>enter message</p>}
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 7 })}
+                  name="Washing Machine days"
+                  onChange={handleWashingDaysChange}
+                  value={washingMachine.days}
+                ></input>
+                <label htmlFor="Washing Machine days">days per week</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
             </div>
           </section>
 
-          <section className="clothes-dryer-sec">
+          <section className="device-section">
             <h3>Clothes Dryer</h3>
-            <div className="dryer-hours">
-              <label htmlFor="Dryer hours">hours</label>
-              <input
-                type="integer"
-                ref={register({ min: 1, max: 24 })}
-                name="Dryer hours"
-                onChange={handleDryerHoursChange}
-                value={dryer.hours}
-              ></input>
-              {errors.entername && <p>enter message</p>}
-            </div>
+            <div className="device-fields">
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 24 })}
+                  name="Dryer hours"
+                  onChange={handleDryerHoursChange}
+                  value={dryer.hours}
+                ></input>
+                <label htmlFor="Dryer hours">hours</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
 
-            <div className="dryer-days">
-              <label htmlFor="Dryer days">days per week</label>
-              <input
-                type="integer"
-                ref={register({ min: 1, max: 7 })}
-                name="Dryer days"
-                onChange={handleDryerDaysChange}
-                value={dryer.days}
-              ></input>
-              {errors.entername && <p>enter message</p>}
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 7 })}
+                  name="Dryer days"
+                  onChange={handleDryerDaysChange}
+                  value={dryer.days}
+                ></input>
+                <label htmlFor="Dryer days">days per week</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
             </div>
           </section>
 
-          <h3>Desktop PC</h3>
-          <label htmlFor="pc hours">hours</label>
-          <input
-            type="integer"
-            ref={register({ min: 1, max: 24 })}
-            name="pc hours"
-            onChange={handlePCHoursChange}
-            value={pc.hours}
-          ></input>
-          {errors.entername && <p>enter message</p>}
+          <section className="device-section">
+            <h3>Desktop PC</h3>
+            <div className="device-fields">
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 24 })}
+                  name="pc hours"
+                  onChange={handlePCHoursChange}
+                  value={pc.hours}
+                ></input>
+                <label htmlFor="pc hours">hours</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 7 })}
+                  name="pc days"
+                  onChange={handlePCDaysChange}
+                  value={pc.days}
+                ></input>
+                <label htmlFor="pc days">days per week</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
+            </div>
+          </section>
 
-          <label htmlFor="pc days">days per week</label>
-          <input
-            type="integer"
-            ref={register({ min: 1, max: 7 })}
-            name="pc days"
-            onChange={handlePCDaysChange}
-            value={pc.days}
-          ></input>
-          {errors.entername && <p>enter message</p>}
-
-          <h3>Television</h3>
-          <label htmlFor="tv hours">hours</label>
-          <input
-            type="integer"
-            ref={register({ min: 1, max: 24 })}
-            name="tv hours"
-            onChange={handleTVHoursChange}
-            value={tv.hours}
-          ></input>
-          {errors.entername && <p>enter message</p>}
-
-          <label htmlFor="tv days">days per week</label>
-          <input
-            type="integer"
-            ref={register({ min: 1, max: 7 })}
-            name="tv days"
-            onChange={handleTVDaysChange}
-            value={tv.days}
-          ></input>
-          {errors.entername && <p>enter message</p>}
+          <section className="device-section">
+            <h3>Television</h3>
+            <div className="device-fields">
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 24 })}
+                  name="tv hours"
+                  onChange={handleTVHoursChange}
+                  value={tv.hours}
+                ></input>
+                <label htmlFor="tv hours">hours</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
+              <div className="device-use-duration">
+                <input
+                  type="integer"
+                  ref={register({ min: 1, max: 7 })}
+                  name="tv days"
+                  onChange={handleTVDaysChange}
+                  value={tv.days}
+                ></input>
+                <label htmlFor="tv days">days per week</label>
+                {errors.entername && <p>enter message</p>}
+              </div>
+            </div>
+          </section>
         </div>
 
         <button type="submit">Confirm</button>
