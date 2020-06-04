49ccf80b6ab026dc32290d4a807f4afb1cf4125f
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-04 18:16:28 -0400

    figured out the switch for the charts and list

--- encon/src/components/mobile/Dashboard.js
+++ encon/src/components/mobile/Dashboard.js
@@ -4,6 +4,9 @@ import { EnergyLocation } from './Energy-Location';
 import { Link, Route } from 'react-router-dom';
 import { ApplianceList } from './Appliance-List';
 import { DesktopGraphs } from '../desktop/Desktop-Graphs';
+import { Graphs } from './Graphs';
+import MediaQuery from 'react-responsive';
+
 export const Dashboard = () => {
   return (
     <div>
@@ -22,7 +25,13 @@ export const Dashboard = () => {
         </div>
       </div>
       <Route path='/profile/appliances' component={ApplianceList} />
-      <Route path='/profile/graphs' component={DesktopGraphs} />
+
+      <MediaQuery minDeviceWidth={1025}>
+        <Route path='/profile/graphs' component={DesktopGraphs} />
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={1025}>
+        <Route path='/profile/graphs' component={Graphs} />
+      </MediaQuery>
     </div>
   );
 };
