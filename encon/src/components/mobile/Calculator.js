d350a68add1045759d9d3eb0eceb806972a48897
Michael Ross <mross.j@gmail.com>
2020-06-03 21:30:46 -0400

    update calculator
    * added dropdown location for state field

--- encon/src/components/mobile/Calculator.js
+++ encon/src/components/mobile/Calculator.js
@@ -14,7 +14,7 @@ export const Calculator = () => {
 	const valueChange = (event) => {
 		setDevice(event.target.value);
 	};
-	const [location, setLocation] = useState('');
+	const [location, setLocation] = useState({ value: 'Select' });
 	const locationChange = (event) => {
 		setLocation(event.target.value);
 	};
@@ -107,66 +107,64 @@ export const Calculator = () => {
 				{/* Location Input */}
 				<h3 className='state'>
 					State
-					<input
-						type='text'
-						name='state'
+					<select
+						className='select-state'
 						onChange={locationChange}
-						value={location}
-					/>
-					{/* <select onChange={locationChange} value={location.value}>
+						value={location.value}
+					>
 						<option value='Select'>Select</option>
-						<option value=' Alabama'> Alabama</option>
-						<option value=' Alaska'> Alaska</option>
-						<option value=' Arizona'> Arizona</option>
-						<option value=' Arkansas'> Arkansas</option>
-						<option value=' California'> California</option>
-						<option value=' Colorado'> Colorado</option>
-						<option value=' Connecticut'> Connecticut</option>
-						<option value=' Delaware'> Delaware</option>
-						<option value=' District Of Columbia'> Washington D.C.</option>
-						<option value=' Florida'> Florida</option>
-						<option value=' Georgia'> Georgia</option>
-						<option value=' Hawaii'> Hawaii</option>
-						<option value=' Idaho'> Idaho</option>
-						<option value=' Illinois'> Illinois</option>
-						<option value=' Indiana'> Indiana</option>
-						<option value=' Iowa'> Iowa</option>
-						<option value=' Kansas'> Kansas</option>
-						<option value=' Kentucky'> Kentucky</option>
-						<option value=' Louisiana'> Louisiana</option>
-						<option value=' Maine'> Maine</option>
-						<option value=' Maryland'> Maryland</option>
-						<option value=' Massachusetts'> Massachusetts</option>
-						<option value=' Michigan'> Michigan</option>
-						<option value=' Minnesota'> Minnesota</option>
-						<option value=' Mississippi'> Mississippi</option>
-						<option value=' Missouri'> Missouri</option>
-						<option value=' Montana'> Montana</option>
-						<option value=' Nebraska'> Nebraska</option>
-						<option value=' Nevada'> Nevada</option>
-						<option value=' New Hampshire'> New Hampshire</option>
-						<option value=' New Jersey'> New Jersey</option>
-						<option value=' New Mexico'> New Mexico</option>
-						<option value=' New York'> New York</option>
-						<option value=' North Carolina'> North Carolina</option>
-						<option value=' North Dakota'> North Dakota</option>
-						<option value=' Ohio'> Ohio</option>
-						<option value=' Oklahoma'> Oklahoma</option>
-						<option value=' Oregon'> Oregon</option>
-						<option value=' Pennsylvania'> Pennsylvania</option>
-						<option value=' Rhode Island'> Rhode Island</option>
-						<option value=' South Carolina'> South Carolina</option>
-						<option value=' South Dakota'> South Dakota</option>
-						<option value=' Tennessee'> Tennessee</option>
-						<option value=' Texas'> Texas</option>
-						<option value=' Utah'> Utah</option>
-						<option value=' Vermont'> Vermont</option>
-						<option value=' Virginia'> Virginia</option>
-						<option value=' Washington'> Washington</option>
-						<option value=' West Virginia'> West Virginia</option>
-						<option value=' Wisconsin'> Wisconsin</option>
-						<option value=' Wyoming '> Wyoming </option>
-					</select> */}
+						<option value='Alabama'>Alabama</option>
+						<option value='Alaska'>Alaska</option>
+						<option value='Arizona'>Arizona</option>
+						<option value='Arkansas'>Arkansas</option>
+						<option value='California'>California</option>
+						<option value='Colorado'>Colorado</option>
+						<option value='Connecticut'>Connecticut</option>
+						<option value='Delaware'>Delaware</option>
+						<option value='District Of Columbia'>Washington D.C.</option>
+						<option value='Florida'>Florida</option>
+						<option value='Georgia'>Georgia</option>
+						<option value='Hawaii'>Hawaii</option>
+						<option value='Idaho'>Idaho</option>
+						<option value='Illinois'>Illinois</option>
+						<option value='Indiana'>Indiana</option>
+						<option value='Iowa'>Iowa</option>
+						<option value='Kansas'>Kansas</option>
+						<option value='Kentucky'>Kentucky</option>
+						<option value='Louisiana'>Louisiana</option>
+						<option value='Maine'>Maine</option>
+						<option value='Maryland'>Maryland</option>
+						<option value='Massachusetts'>Massachusetts</option>
+						<option value='Michigan'>Michigan</option>
+						<option value='Minnesota'>Minnesota</option>
+						<option value='Mississippi'>Mississippi</option>
+						<option value='Missouri'>Missouri</option>
+						<option value='Montana'>Montana</option>
+						<option value='Nebraska'>Nebraska</option>
+						<option value='Nevada'>Nevada</option>
+						<option value='New Hampshire'>New Hampshire</option>
+						<option value='New Jersey'>New Jersey</option>
+						<option value='New Mexico'>New Mexico</option>
+						<option value='New York'>New York</option>
+						<option value='North Carolina'>North Carolina</option>
+						<option value='North Dakota'>North Dakota</option>
+						<option value='Ohio'>Ohio</option>
+						<option value='Oklahoma'>Oklahoma</option>
+						<option value='Oregon'>Oregon</option>
+						<option value='Pennsylvania'>Pennsylvania</option>
+						<option value='Rhode Island'>Rhode Island</option>
+						<option value='South Carolina'>South Carolina</option>
+						<option value='South Dakota'>South Dakota</option>
+						<option value='Tennessee'>Tennessee</option>
+						<option value='Texas'>Texas</option>
+						<option value='Utah'>Utah</option>
+						<option value='Vermont'>Vermont</option>
+						<option value='Virginia'>Virginia</option>
+						<option value='Washington'>Washington</option>
+						<option value='West Virginia'>West Virginia</option>
+						<option value='Wisconsin'>Wisconsin</option>
+						<option value='Wyoming '>Wyoming</option>
+					</select>
 				</h3>
 				<h3 className='hourly-use'>Hourly Use</h3>
 				{/* Hourly Use Container for Hours and Minutes */}
