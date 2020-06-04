de32807aefd4b011d05bf0bf51b7f0e040684a6d
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-04 12:20:36 -0400

    made second graph still styling

--- encon/src/components/mobile/Graphs.js
+++ encon/src/components/mobile/Graphs.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import '../../styles/mobile/Graph.scss';
 import {
   LineChart,
+  BarChart,
   Line,
+  Bar,
+  Label,
   CartesianGrid,
   XAxis,
   YAxis,
@@ -18,25 +22,55 @@ export const UserGraph = () => {
   ];
 
   return (
-    <LineChart
-      width={1000}
-      height={300}
-      data={data}
-      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-    >
-      <Line type='monotone' dataKey='TV' stroke='red' strokeWidth={2} />
-      <Line type='monotone' dataKey='Computer' stroke='blue' strokeWidth={2} />
-      <Line
-        type='monotone'
-        dataKey='Microwave'
-        stroke='purple'
-        strokeWidth={2}
-      />
-      <CartesianGrid stroke='grey' strokeDasharray='10' />
-      <XAxis dataKey='month' />
-      <YAxis />
-      <Tooltip content={data} />
-      <Legend />
-    </LineChart>
+    <div className='graphContainer'>
+      <LineChart
+        width={1000}
+        height={500}
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type='monotone' dataKey='TV' stroke='red' strokeWidth={2} />
+        <Line
+          type='monotone'
+          dataKey='Computer'
+          stroke='blue'
+          strokeWidth={2}
+        />
+        <Line
+          type='monotone'
+          dataKey='Microwave'
+          stroke='purple'
+          strokeWidth={2}
+        />
+        <CartesianGrid stroke='grey' strokeDasharray='10' />
+        <XAxis dataKey='month'/>
+        <YAxis>
+            <Label value="Month" position="left" offset={1}/>
+        </YAxis>
+        <Tooltip content={data} />
+        <Legend />
+      </LineChart>
+
+      <BarChart
+        width={1000}
+        height={500}
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <CartesianGrid strokeDasharray='10' stroke='grey' />
+        <XAxis dataKey='month' />
+        <YAxis/>
+        <Tooltip content={data} />
+        <Legend />
+        <Bar type='monotone' dataKey='TV' fill='red' strokeWidth={2} />
+        <Bar type='monotone' dataKey='Computer' fill='blue' strokeWidth={2} />
+        <Bar
+          type='monotone'
+          dataKey='Microwave'
+          fill='purple'
+          strokeWidth={2}
+        />
+      </BarChart>
+    </div>
   );
 };
