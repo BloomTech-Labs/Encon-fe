786fd480af27e28266d3eece79abf1d31d8988d7
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-10 16:48:37 -0400

    made graph tests and rewrote LP tests

--- encon/src/tests/Landing-Page.test.js
+++ encon/src/tests/Landing-Page.test.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getByText } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { LandingPage } from '.././components/mobile/Landing-Page';
 import { Header } from '.././components/mobile/Header';
 import { Calculator } from '.././components/mobile/Calculator';
 
-describe('testing the landing page component', () => {
-  test('should render the landing page correctly ', () => {
-    render(
-      <Router>
-        <LandingPage />
-      </Router>
-    );
-  });
-  test('should render the header component ', () => {
-    render(
-      <Router>
-        <LandingPage />
-        <Header />
-      </Router>
-    );
-  });
-  test('should render the calculator component ', () => {
-    render(
-      <Router>
-        <LandingPage />
-        <Calculator />
-      </Router>
-    );
-  });
+test('should render the landing page correctly ', () => {
+  const { getByText } = render(
+    <Router>
+      <LandingPage />
+    </Router>
+  );
+  const headerText = getByText('Welcome to EnCon!');
+  expect(headerText).toBeTruthy();
 });
