49f5d62c2cf8cf28b7783c001a272e3e30bfe10a
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-09 20:10:45 -0400

    wokring on calculator testing

--- encon/src/tests/Calculator.test.js
+++ encon/src/tests/Calculator.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Calculator } from "../components/Calculator";
-import { BrowserRouter as Router } from "react-router-dom";
-test("should render Calculator component ", () => {
+import React from 'react';
+import { render, act, fireEvent } from '@testing-library/react';
+import { Calculator } from '../components/Calculator';
+import { BrowserRouter as Router } from 'react-router-dom';
+test('should render Calculator component ', () => {
   render(
     <Router>
       <Calculator />
@@ -10,7 +10,7 @@ test("should render Calculator component ", () => {
   );
 });
 
-test("check for text", () => {
+test('check for text', () => {
   const { getByText } = render(
     <Router>
       <Calculator />
@@ -18,4 +18,29 @@ test("check for text", () => {
   );
   const register = getByText(/register/i);
   expect(register).toBeInTheDocument();
+});
+
+test('should show the correct outputs given inputs', () => {
+  const { getByLabelText } = render(
+    <Router>
+      <Calculator />
+    </Router>
+  );
+  const deviceButton = getByLabelText(/device/i);
+  const stateInput = getByLabelText(/state/i);
+  const hoursInput = getByLabelText(/hourly use/i);
+  const daysInput = getByLabelText(/days per week used/i);
+
+  act(() => {
+    fireEvent.click(deviceButton);
+    fireEvent.change(stateInput, {
+      target: { name: 'State', value: 'Virginia' },
+    });
+    fireEvent.change(hoursInput, {
+      target: { name: 'Hourly Use', value: '5' },
+    });
+    fireEvent.change(daysInput, {
+      target: { name: 'Days Per Week Used', value: '5' },
+    });
+  });
 });
