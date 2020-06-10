57ef932554d8396f0c4549b5919f59114bf371fa
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-10 13:36:37 -0400

    updated import paths

--- encon/src/tests/Landing-Page.test.js
+++ encon/src/tests/Landing-Page.test.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { LandingPage } from "../components/Landing-Page";
-import { Header } from "../components/Header";
-import { Calculator } from "../components/Calculator";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { LandingPage } from '.././components/mobile/Landing-Page';
+import { Header } from '.././components/mobile/Header';
+import { Calculator } from '.././components/mobile/Calculator';
 
-describe("testing the landing page component", () => {
-  test("should render the landing page correctly ", () => {
+describe('testing the landing page component', () => {
+  test('should render the landing page correctly ', () => {
     render(
       <Router>
         <LandingPage />
       </Router>
     );
   });
-  test("should render the header component ", () => {
+  test('should render the header component ', () => {
     render(
       <Router>
         <LandingPage />
@@ -21,7 +21,7 @@ describe("testing the landing page component", () => {
       </Router>
     );
   });
-  test("should render the calculator component ", () => {
+  test('should render the calculator component ', () => {
     render(
       <Router>
         <LandingPage />
