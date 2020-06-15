b8ad185c7f24a36e1218cfbbffe18049091dfa17
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-15 12:44:12 -0400

    fixed appliance/tacker stlying and formatted code

--- encon/src/components/mobile/Dashboard.js
+++ encon/src/components/mobile/Dashboard.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { WelcomeHeader } from "./Welcome-Header";
-import { EnergyLocation } from "./Energy-Location";
-import { Link, Route } from "react-router-dom";
-import { ApplianceList } from "./Appliance-List";
-import { DesktopGraphs } from "../desktop/Desktop-Graphs";
-import { Graphs } from "./Graphs";
-import MediaQuery from "react-responsive";
-
+import React from 'react';
+import { WelcomeHeader } from './Welcome-Header';
+import { EnergyLocation } from './Energy-Location';
+import { Link, Route } from 'react-router-dom';
+import { ApplianceList } from './Appliance-List';
+import { DesktopGraphs } from '../desktop/Desktop-Graphs';
+import { Graphs } from './Graphs';
+import MediaQuery from 'react-responsive';
+import '../../styles/mobile/Appliance-Tracker.scss';
 export const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <div className='dashboard'>
       <WelcomeHeader />
       <EnergyLocation />
-      <div className="applianceTracker">
-        <div className="left">
-          <Link to="/profile/appliances" className="leftButton">
+      <div className='applianceTracker'>
+        <div className='left'>
+          <Link to='/profile/appliances' className='leftButton'>
             Appliance List
           </Link>
         </div>
-        <div className="right">
-          <Link to="/profile/graphs" className="rightButton">
+        <div className='right'>
+          <Link to='/profile/graphs' className='rightButton'>
             Track Usage
           </Link>
         </div>
       </div>
-      <Route path="/profile/appliances" component={ApplianceList} />
+      <Route path='/profile/appliances' component={ApplianceList} />
 
       <MediaQuery minDeviceWidth={1025}>
-        <Route path="/profile/graphs" component={DesktopGraphs} />
+        <Route path='/profile/graphs' component={DesktopGraphs} />
       </MediaQuery>
       <MediaQuery maxDeviceWidth={1025}>
-        <Route path="/profile/graphs" component={Graphs} />
+        <Route path='/profile/graphs' component={Graphs} />
       </MediaQuery>
     </div>
   );
