73f500ef4fda842155ae8574b56d2f2a5eba9033
Alex Martinez <alexmartinez0071234@gmail.com>
2020-06-23 12:25:49 -0400

    missed one s in http for base url

--- encon/src/utils/auth/axiosWithAuth.js
+++ encon/src/utils/auth/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
   const AUTH_TOKEN = localStorage.getItem('AUTH_TOKEN');
   return axios.create({
-    baseURL: 'https://localhost:3300/api',
+    baseURL: 'http://localhost:3300/api',
     headers: {
       'Content-Type': 'application/json',
       Authorization: AUTH_TOKEN,
